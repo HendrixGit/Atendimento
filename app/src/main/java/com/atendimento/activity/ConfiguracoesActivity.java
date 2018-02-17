602db5ab2ee9535a8ba7221afef812407b5c614c
Hendrix <brunosalustiano303@hotmail.com>
2018-02-16 23:10:16 -0200

    Início activity Configurações

---
+++ app/src/main/java/com/atendimento/activity/ConfiguracoesActivity.java
@@ -0,0 +1,15 @@
+package com.atendimento.activity;
+
+import android.os.Bundle;
+
+import com.atendimento.R;
+import com.atendimento.bases.BaseActivity;
+
+public class ConfiguracoesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes);
+    }
+}
