82257e93ca48643a3493d35762464ae098180d8a
HendrixGit <brunosalustiano303@hotmail.com>
2018-05-08 23:06:06 -0300

    Banco SQlite Categorias

--- app/src/main/java/com/atendimento/activity/CadastrarEmpresaActivity.java
+++ app/src/main/java/com/atendimento/activity/CadastrarEmpresaActivity.java
@@ -1,15 +1,17 @@
 package com.atendimento.activity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
-
 import com.atendimento.R;
 import com.atendimento.bases.BaseActivity;
+import com.atendimento.util.Util;
 import com.google.firebase.database.DatabaseReference;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,17 +28,41 @@ public class CadastrarEmpresaActivity extends BaseActivity {
     private Spinner spinnerCategoria;
     private List<String> listaCategoria;
     private ArrayAdapter<String> dataCategoria;
+    private EditText nomeEmpresa;
+    private Util util;
+    private SQLiteDatabase sqLiteDatabasePar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_empresa);
+        nomeEmpresa      = findViewById(R.id.editTextNomeEmpresa);
+        util = new Util();
         spinnerCategoria = findViewById(R.id.spinnerCategoria);
+        circleImageView = findViewById(R.id.circleImageEmpresa);
         listaCategoria = new ArrayList<String>();
-        listaCategoria.add("MK");
+
+        sqLiteDatabasePar = openOrCreateDatabase("databaseCategorias", MODE_PRIVATE, null);
+        sqLiteDatabasePar.execSQL("DELETE FROM categorias");
+        sqLiteDatabasePar.execSQL("CREATE TABLE IF NOT EXISTS categorias(codigo INT(3), descricao VARCHAR)");
+        sqLiteDatabasePar.execSQL("INSERT INTO categorias(codigo, descricao)  VALUES(1, 'Clínicas Médicas')");
+        sqLiteDatabasePar.execSQL("INSERT INTO categorias(codigo, descricao)  VALUES(2, 'Pet Shops')");
+        sqLiteDatabasePar.execSQL("INSERT INTO categorias(codigo, descricao)  VALUES(3, 'Laboratórios')");
+        sqLiteDatabasePar.execSQL("INSERT INTO categorias(codigo, descricao)  VALUES(4, 'Manicure/Pedicuere')");
+        sqLiteDatabasePar.execSQL("INSERT INTO categorias(codigo, descricao)  VALUES(5, 'Salões de Beleza')");
+        sqLiteDatabasePar.execSQL("INSERT INTO categorias(codigo, descricao)  VALUES(6, 'Escritórios Advocacia')");
+
+        Cursor cursor = sqLiteDatabasePar.rawQuery("SELECT codigo, descricao FROM categorias",null);
+        int indiceColunaDescricao = cursor.getColumnIndex("descricao");
+        cursor.moveToFirst();
+
+        while (!cursor.isLast()) {
+            listaCategoria.add(cursor.getString(indiceColunaDescricao));
+            cursor.moveToNext();
+        }
+
         dataCategoria = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, listaCategoria);
         spinnerCategoria.setAdapter(dataCategoria);
-
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Cadastro de Empresa");
         toolbar.setTitleTextColor(getResources().getColor(R.color.colorBranco));
@@ -59,6 +85,13 @@ public class CadastrarEmpresaActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 mudarTelaFinish(getApplicationContext(),MainActivity.class);
+            }
+        });
+
+        circleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
--- app/src/main/java/com/atendimento/util/Util.java
+++ app/src/main/java/com/atendimento/util/Util.java
@@ -3,19 +3,17 @@ package com.atendimento.util;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+
 import com.atendimento.R;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class Util{
 
-    private SQLiteDatabase databaseAtendimento;
+    private String sql;
 
     public String returnDataString(Date data) throws ParseException {
         long now = System.currentTimeMillis();
@@ -53,10 +51,10 @@ public class Util{
         return resizedBitmap;
     }
 
-    public AlertDialog.Builder YesNoDialog(String titulo, Context context, DialogInterface.OnClickListener onClickListener){
+    public AlertDialog.Builder YesNoDialog(String titulo, Context context, DialogInterface.OnClickListener onClickListenerYes){
         AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.dialog);
         builder.setTitle(titulo);
-        builder.setPositiveButton("Sim", onClickListener);
+        builder.setPositiveButton("Sim", onClickListenerYes);
         builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -65,23 +63,4 @@ public class Util{
         });
         return builder;
     }
-
-    public String categoriasDatabase(int codigo){
-        databaseAtendimento = SQLiteDatabase.openOrCreateDatabase("database", null);
-
-        databaseAtendimento.execSQL("CREATE TABLE IF NOT EXISTS categorias(codigo INT(3),descricao VARCHAR) ");
-        databaseAtendimento.execSQL("INSERT INTO categorias(codigo,descricao) VALUES('Clínicas Médicas',1)");
-        databaseAtendimento.execSQL("INSERT INTO categorias(codigo,descricao) VALUES('Pet Shops',2)");
-        databaseAtendimento.execSQL("INSERT INTO categorias(codigo,descricao) VALUES('Laboratórios',3)");
-        databaseAtendimento.execSQL("INSERT INTO categorias(codigo,descricao) VALUES('Manicure/Pedicuere',4)");
-        databaseAtendimento.execSQL("INSERT INTO categorias(codigo,descricao) VALUES('Salões de Beleza',5)");
-        databaseAtendimento.execSQL("INSERT INTO categorias(codigo,descricao) VALUES('Escritórios Advocacia',6)");
-
-        Cursor cursor = databaseAtendimento.rawQuery("SELECT * FROM categorias WHERE codigo = " + codigo,null);
-        int indiceColunaDescricao = cursor.getColumnIndex("descricao");
-        cursor.moveToFirst();
-
-        return cursor.getString(indiceColunaDescricao);
-    }
-
 }
