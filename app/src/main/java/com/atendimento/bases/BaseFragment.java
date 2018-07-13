35b942857fd4bcad5e44f9bc818e5c8959b1e51d
HendrixGit <brunosalustiano303@hotmail.com>
2018-07-12 22:48:05 -0300

    Base Fragment II

--- app/src/main/java/com/atendimento/bases/BaseFragment.java
+++ app/src/main/java/com/atendimento/bases/BaseFragment.java
@@ -1,23 +1,45 @@
 package com.atendimento.bases;
-
-
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.atendimento.R;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.Query;
+import com.google.firebase.storage.StorageReference;
 
 import java.io.Serializable;
 
 
 public class BaseFragment extends Fragment {
 
-    private ListView listView;
-    private ArrayAdapter arrayAdapter;
-    private DatabaseReference firebaseDatabase;
-    private ValueEventListener valueEventListener;
+    protected View viewBase;
+    protected RecyclerView recyclerViewBase;
+    protected FloatingActionButton floatingActionButtonBase;
+    protected Boolean modoSelecaoBase = false;
+    protected Integer selecionadosBase = 0;
+    protected Query queryBase;
+    protected DatabaseReference firebaseDatabase;
+    protected ChildEventListener childEventListenerBase;
+    protected StorageReference storageReferenceBase;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        viewBase         = inflater.inflate(R.layout.fragment_empresas, container, false);
+        recyclerViewBase = viewBase.findViewById(R.id.recyclerViewEmpresas);
+        floatingActionButtonBase =  viewBase.findViewById(R.id.floatButtonCadastrarEmpresa);
+        return viewBase;
+    }
 
     protected void mudarTela(Context contexto, Class classe) {
         Intent intent = new Intent(contexto, classe);
