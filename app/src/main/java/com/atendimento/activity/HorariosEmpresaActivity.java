ac192705875c7c94d67128fbd7f53a881ec264f7
HendrixGit <brunosalustiano303@hotmail.com>
2018-07-29 19:57:26 -0300

    Dias semana strings

--- app/src/main/java/com/atendimento/activity/HorariosEmpresaActivity.java
+++ app/src/main/java/com/atendimento/activity/HorariosEmpresaActivity.java
@@ -13,10 +13,6 @@ import com.atendimento.fragment.HorarioDialog;
 import com.atendimento.util.MyDialogFragmentListener;
 import com.atendimento.util.Util;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
 
 public class HorariosEmpresaActivity extends BaseActivity implements MyDialogFragmentListener {
 
@@ -43,9 +39,6 @@ public class HorariosEmpresaActivity extends BaseActivity implements MyDialogFra
         toolbarBase = findViewById(R.id.toolbar);
         toolbarBase.setTitle("Cadastrar Horários");
         toolbarBase.setTitleTextColor(getResources().getColor(R.color.colorBranco));
-        setSupportActionBar(toolbarBase);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
 
         util = new Util();
         inicio = findViewById(R.id.textViewInicio);
@@ -59,20 +52,26 @@ public class HorariosEmpresaActivity extends BaseActivity implements MyDialogFra
         sabado  = findViewById(R.id.checkBoxSabado);
         domingo = findViewById(R.id.checkBoxDomingo);
 
-
-        DateFormat dateFormat = new SimpleDateFormat("EEEE");
-        Calendar calendar = Calendar.getInstance();
-
-
-        segunda.setText("Segunda-Feira");
-
+        segunda.setText(R.string.segunda);
+        segunda.setChecked(true);
+        terca.setText(R.string.terca);
+        terca.setChecked(true);
+        quarta.setText(R.string.quarta);
+        quarta.setChecked(true);
+        quinta.setText(R.string.quinta);
+        quinta.setChecked(true);
+        sexta.setText(R.string.sexta);
+        sexta.setChecked(true);
+        sabado.setText(R.string.sabado);
+        sabado.setChecked(true);
+        domingo.setText(R.string.domingo);
 
         horaInicio = findViewById(R.id.buttonHorarioInicio);
         horaInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 op = false;
-                setHorariosTextViews(inicio,getResources().getString(R.string.inicio));
+                setHorariosTextViews(inicio,getResources().getString(R.string.horarioInicial));
             }
         });
 
@@ -81,7 +80,7 @@ public class HorariosEmpresaActivity extends BaseActivity implements MyDialogFra
             @Override
             public void onClick(View view) {
                 op = true;
-                setHorariosTextViews(fim,getResources().getString(R.string.Fim));
+                setHorariosTextViews(fim,getResources().getString(R.string.horarioInicial));
             }
         });
 
@@ -103,7 +102,7 @@ public class HorariosEmpresaActivity extends BaseActivity implements MyDialogFra
     @Override
     public void onReturnValue(String resultadoParametro) {
         if (!resultadoParametro.equals("")) {
-            if (op == false) {
+            if (!op) {
                 inicio.setText(resultadoParametro);
             }
             else{
