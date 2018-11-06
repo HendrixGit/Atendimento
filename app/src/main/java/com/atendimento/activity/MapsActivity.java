677f9458141dcbd1d3a0cef3debcdbc85c3f91f7
HendrixGit <brunosalustiano303@hotmail.com>
2018-11-05 23:57:10 -0200

    Formas maps I

--- app/src/main/java/com/atendimento/activity/MapsActivity.java
+++ app/src/main/java/com/atendimento/activity/MapsActivity.java
@@ -1,14 +1,18 @@
 package com.atendimento.activity;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.atendimento.R;
+import com.atendimento.config.ConfiguracaoFirebase;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -41,11 +45,55 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         LatLng vot = new LatLng(-20.415768, -49.974491);
         mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        CircleOptions circleOptions = new CircleOptions();
+        circleOptions.center(vot);
+        circleOptions.radius(500);//em metros
+        circleOptions.fillColor(Color.BLUE);
+        circleOptions.fillColor(Color.argb(82, 119, 188, 1));
+        circleOptions.strokeWidth(10);
+        circleOptions.strokeColor(Color.BLACK);
+
+        mMap.addCircle(circleOptions);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                Double latitude  = latLng.latitude;
+                Double longitude = latLng.longitude;
+
+                Toast.makeText(getApplicationContext(), latitude + " - " + longitude, Toast.LENGTH_SHORT ).show();
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("onClick")
+                        .snippet("Description")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_action_user)));
+            }
+        });
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                Double latitude  = latLng.latitude;
+                Double longitude = latLng.longitude;
+
+                Toast.makeText(getApplicationContext(), latitude + " - " + longitude, Toast.LENGTH_SHORT ).show();
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title("onLongClick")
+                        .snippet("Description")
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_action_user)));
+            }
+        });
+
         mMap.addMarker(new MarkerOptions().position(vot).title("Marker")
                 .icon(BitmapDescriptorFactory.fromResource(R.drawable.close)));
 
-                //icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+        //icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
         //zoom de 2.0 ate 21.0
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vot, 18));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vot, 15));
     }
 }
