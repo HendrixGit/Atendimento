5f58399ae5e32cd4a1b9a552abe0c18ebb7d8c2f
HendrixGit <brunosalustiano303@hotmail.com>
2018-11-15 20:51:55 -0200

    Pesquisa mapa

--- app/src/main/java/com/atendimento/activity/MapaActivity.java
+++ app/src/main/java/com/atendimento/activity/MapaActivity.java
@@ -1,21 +1,41 @@
 package com.atendimento.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.atendimento.R;
 import com.atendimento.bases.BaseActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapaActivity extends BaseActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private LocationManager locationManager;
     private LocationListener locationListener;
+    private EditText destino;
+    private ImageView imageViewPesquisarEndereco;
+    private TextView minhaLocalizacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +45,30 @@ public class MapaActivity extends BaseActivity implements OnMapReadyCallback {
         toolbarBase = findViewById(R.id.toolbar);
         toolbarBase.setTitle("Definir Local Empresa");
         toolbarBase.setTitleTextColor(getResources().getColor(R.color.colorBranco));
+        destino = findViewById(R.id.editTextPesqEndereco);
+        destino.setCursorVisible(false);
+        destino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                destino.setCursorVisible(true);
+            }
+        });
+        minhaLocalizacao = findViewById(R.id.textViewPesquisaEndereco);
+        minhaLocalizacao.setText(" " + getResources().getString(R.string.localizacao));
+        minhaLocalizacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recuperaLocalizacao();
+            }
+        });
+
+        imageViewPesquisarEndereco = findViewById(R.id.imageViewPesqEndereco);
+        imageViewPesquisarEndereco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buscarEndereco(destino.getText().toString());
+            }
+        });
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapa);
         mapFragment.getMapAsync(this);
@@ -32,6 +76,89 @@ public class MapaActivity extends BaseActivity implements OnMapReadyCallback {
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        recuperaLocalizacao();
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    private void buscarEndereco(String endereco){
+        if (!destino.getText().equals("") || destino.getText() != null){
+            Address enderecoFinal = recuperarEndereco(endereco);
+            if (enderecoFinal != null){
+                Double latitude  = enderecoFinal.getLatitude();
+                Double longitude = enderecoFinal.getLongitude();
+                adcionarMarcador("Destino", latitude, longitude);
+            }
+        }
+    }
+
+    private Address recuperarEndereco(String endereco){
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> listaEnderecos = geocoder.getFromLocationName(endereco, 1);
+            if (listaEnderecos != null && listaEnderecos.size() > 0){
+                Address address = listaEnderecos.get(0);
+                return address;
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void recuperaLocalizacao() {
+        locationManager = (LocationManager) this.getSystemService(getApplicationContext().LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Double latitude  = location.getLatitude();
+                Double longitude = location.getLongitude();
+
+                adcionarMarcador("Minha Localização", latitude, longitude);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    1000,
+                    10,
+                    locationListener
+            );
+        }
+    }
+    private void adcionarMarcador(String descLocalizacao, Double parLatitude, Double parLongitude){
+        LatLng localizacao = new LatLng(parLatitude, parLongitude);
+
+        mMap.clear();
+        mMap.addMarker(
+                new MarkerOptions()
+                        .position(localizacao)
+                        .title(descLocalizacao)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_action_user))
+        );
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localizacao, 18));
     }
 }
